a8183f49ab28dd2c5133f4debb9e9e1d8bde1926
Seenivasan Sankaran <mailtonivass@gmail.com>
2019-01-07 14:49:39 +0530

    Added Project

--- src/main/java/com/altimetrik/spring/service/ITaskPreferenceService.java
+++ src/main/java/com/altimetrik/spring/service/ITaskPreferenceService.java
@@ -1,12 +1,8 @@
 package com.altimetrik.spring.service;
 
 import com.altimetrik.spring.modal.TaskPreference;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface ITaskPreferenceService {
 
     void updateTask(String task, Long picked_time);
